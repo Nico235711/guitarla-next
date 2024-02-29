a58d8efffc31a90fea932a60aeeb9f2966082c01
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-02-29 17:57:48 -0300

    Creando la página de Nosotros y crear imágenes avif y webp con Next Future Image

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,5 +1,6 @@
+import Image from "next/image"
 import Layout from "../components/layout"
-
+import styles from '../styles/nosotros.module.css'
 
 const Nosotros = () => {
   
@@ -8,7 +9,17 @@ const Nosotros = () => {
       title="Nosotros"
       description="Sobre nosotros, guitarLA, tienda de música"
     >
-      <h1>Nosotros</h1>
+      <main className="contenedor">
+        <h2 className="heading">Nosotros</h2>
+
+        <div className={styles.contenido}>
+          <Image src="/img/nosotros.jpg" width={1000} height={800} alt="Imagen sobre nosotros" />
+
+          <div>
+            <p>Maecenas ut lacinia est, eget mollis mauris. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi at ligula id nisi tincidunt molestie. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras elementum dui felis, ac malesuada tortor efficitur sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam</p>
+          </div>
+        </div>
+      </main>
     </Layout>
   )
 }
